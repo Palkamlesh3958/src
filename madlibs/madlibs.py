a4c4f0996b9a5f3fa67a581de15c34ae7ca26c55
Palkamlesh3958 <kamleshpal3958@gmail.com>
2022-04-20 16:25:53 -0700

    added madlibs solution

---
+++ madlibs/madlibs.py
@@ -0,0 +1,76 @@
+"""A madlib game that compliments its users."""
+
+from random import choice
+
+from flask import Flask, render_template, request
+
+# "__name__" is a special Python variable for the name of the current module.
+# Flask wants to know this to know what any imported things are relative to.
+app = Flask(__name__)
+
+AWESOMENESS = [
+    "awesome",
+    "terrific",
+    "fantastic",
+    "neato",
+    "fantabulous",
+    "wowza",
+    "oh-so-not-meh",
+    "brilliant",
+    "ducky",
+    "coolio",
+    "incredible",
+    "wonderful",
+    "smashing",
+    "lovely",
+]
+
+
+@app.route("/")
+def start_here():
+    """Display homepage."""
+
+    return "Hi! This is the home page."
+
+
+@app.route("/hello")
+def say_hello():
+    """Say hello to user."""
+
+    return render_template("hello.html")
+
+
+@app.route("/greet")
+def greet_person():
+    """Greet user with compliment."""
+
+    player = request.args.get("person")
+
+    compliment = choice(AWESOMENESS)
+
+    return render_template("compliment.html", person=player, compliment=compliment)
+
+@app.route("/game")
+def show_madlib_form():
+    """Ask user response for game."""
+    usr_response = request.args.get("game")
+    if usr_response == "Yes":
+        return render_template("game.html")
+    else:
+        return render_template("goodbye.html")
+
+@app.route("/madlib")
+def show_madlib():
+    """Display user's Madlibs-style story."""
+    person_name = request.args.get("person")
+    col_choice = request.args.get("color")
+    usr_noun = request.args.get("noun")
+    usr_adj = request.args.get("adjective")
+
+    return render_template("madlib.html", person=person_name, color=col_choice, noun=usr_noun, adjective=usr_adj)
+
+if __name__ == "__main__":
+    # Setting debug=True gives us error messages in the browser and also
+    # "reloads" our web app if we change the code.
+
+    app.run(debug=True, host="0.0.0.0")
