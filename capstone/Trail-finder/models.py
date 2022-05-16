4966004fbec49a41c0bf3a798c1a5c8ca36c7d5d
Palkamlesh3958 <kamleshpal3958@gmail.com>
2022-05-16 05:25:19 -0700

    Added data.json and updated models.py and server.py

--- capstone/Trail-finder/models.py
+++ capstone/Trail-finder/models.py
@@ -1,19 +1,71 @@
+"""Models for trail finder app."""
+
 from datetime import datetime
 from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
 class User(db.Model):
-    """A user."""
+    """Users."""
 
     __tablename__ = "users"
 
-    user_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
-    first_name = db.Column(db.String)
-    last_name = db.Column(db.String)
-    email = db.Column(db.String, unique=True)
-    password = db.Column(db.String)
-    confirm_password = db.Column(db.String)
+    user_id = db.Column(db.Integer, autoincrement=True, primary_key=True, nullable=False)
+    first_name = db.Column(db.String(64), nullable=False)
+    last_name = db.Column(db.String(64), nullable=True)
+    username = db.Column(db.String(128), nullable=False)
+    email = db.Column(db.String(128), nullable=False, unique=True)
+    password = db.Column(db.String(64), nullable=False)
+    confirm_password = db.Column(db.String(64), nullable=False)
 
     def __repr__(self):
-        return f
+        """Show user details when printed."""
+        return f"""<User user_id={self.user_id} 
+                    username={self.username} 
+                    email={self.email}>"""
+
+
+# class User(db.model):
+#     """A user."""
+
+#     __tablename__ = "users"
+
+#     user_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
+#     email = db.Column(db.String, unique=True)
+#     pasword = db.Column(db.String)
+
+#     def __repr__(self):
+#         return f<"User user_id={self.user_id} email={self.email}">
+
+
+# def connect_to_db(flask_app, db_uri="postgresql:///trailfinder", echo=True):
+#     flask_app.config["SQLALCHEMY_DATABASE_URI"] = db_uri
+#     flask_app.config["SQLALCHEMY_ECHO"] = True
+#     flask_app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+#     db.app = flask_app
+#     db.init_app(flask_app)
+
+#     print("Connected to the db!")
+
+def connect_to_db(app, db_name):
+    """Connect to database."""
+
+    app.config["SQLALCHEMY_DATABASE_URI"] = f"postgresql:///{db_name}"
+    app.config["SQLALCHEMY_ECHO"] = True
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+    db.app = app
+    db.init_app(app)
+
+    print("Connected to the db!")
+
+
+if __name__ == "__main__":
+    from server import app
+
+    # Call connect_to_db(app, echo=False) if your program output gets
+    # too annoying; this will tell SQLAlchemy not to print out every
+    # query it executes.
+
+    connect_to_db(app, "trailfinder")
--- capstone/Trail-finder/server.py
+++ capstone/Trail-finder/server.py
@@ -1,6 +1,13 @@
 from flask import Flask, render_template, request, flash, session, redirect
+from model import connect_to_db, db
+# import curd
+
+from jinja2 import StrictUndefined
 
 app = Flask(__name__)
+app.secret_key = "mycapstoneproject"
+app.jinja_env.undefined = StrictUndefined
+
 
 @app.route("/")
 def homepage():
@@ -30,7 +37,7 @@ def submit():
         password = request.form.get('password')
 
     if email == '' or password == '':
-        return render_template('login.html', message = "Please enter required fields")
+       return render_template('login.html', message = "Please enter required fields")
 
     return render_template('success.html')
 
@@ -46,7 +53,29 @@ def register():
     if first_name == '' or last_name == '' or email_add == '' or password == '' or confirm_password == '':
         return render_template('sign_up.html', message = "Please enter all required fields")
 
-    return render_template('success.html')
+    user = curd.get_user_by_email(email_add)
+    if user:
+        flash("Cannot create an account with that email. User already exists. Try again.")
+    else:
+        user = crud.create_user(first_name, last_name, email_add, password)
+        db.session.add(user)
+        db.session.commit()
+        flash("Account created! Please log in.")
+    # return render_template('success.html')
+    return redirect("/")
 
+
+
+# Check this function
+@app.route("/logout", methods=['POST'])
+def logout():
+    """User logout"""
+
+    return render_template(url_for(homepage)) 
+    
+# Create comment
+
+# Updat comment
+# Delete comment
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True)
